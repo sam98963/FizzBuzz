191b3df51deb596dc302971443f3af7991aac6db
sam98963 <szwilliamson01@gmail.com>
2023-08-11 09:54:21 +0200

    plan

--- fizzbuzz_game/src/App.js
+++ fizzbuzz_game/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
+
+
+// Render 3 buttons - fizz, buzz and fizzbuzz.
+// function handle api calls with relevent endpoints
+// Display numbers and relevant transformed numbers in small boxes on screen - 1-100.
 
 export default App;
---
+++ server/index.php
@@ -0,0 +1,3 @@
+<!-- Establish 3 api calls - one for the fizz numbers, one buzz and one for fizzbuzz? -->
+<!-- Allow access control from localhost:3000 for frontend to request from this server.-->
+<!-- Create a Json file with all numbers in. -->
