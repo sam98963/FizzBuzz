a69471666e9272609be131d8d4676e8315775c1f
sam98963 <szwilliamson01@gmail.com>
2023-08-11 10:01:46 +0200

    connecting the front-end and backend servers

--- fizzbuzz_game/src/App.js
+++ fizzbuzz_game/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-
+import useEffect from "react"
 function App() {
+  // On page render (on mount) => get request.
 }
 
 
--- server/index.php
+++ server/index.php
@@ -1,3 +1,19 @@
+<!-- Test Server connection -->
 <!-- Establish 3 api calls - one for the fizz numbers, one buzz and one for fizzbuzz? -->
 <!-- Allow access control from localhost:3000 for frontend to request from this server.-->
 <!-- Create a Json file with all numbers in. -->
+
+<?php 
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  // Return a successful response code.
+  http_response_code(200);
+  exit();
+}
+// response object
+$response = array("message"=>"Server Request Successful");
+// Allow Json response
+header("Content-type: application/json");
+// Object to Json
+echo json_encode($response)
+?>
